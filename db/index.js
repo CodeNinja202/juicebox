ee0815f78d5e878aa8880753aa5f774412e061dc
Ross <rosskeene196@gmail.com>
2022-09-22 19:59:44 -0500

    update user

--- db/index.js
+++ db/index.js
@@ -1,41 +1,35 @@
-const { Client } = require('pg'); // imports the pg module
+const { Client } = require('pg') // imports the pg module
 
-// supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
-
-async function getAllUsers() {
-  const { rows } = await client.query(
-    `SELECT id, username 
-    FROM users;
-  `);
-
-  return rows;
-}
-
-// in db/index.js
-
-
-async function createUser({ username, password }) {
+async function createUser({ 
+  username, 
+  password,
+  name,
+  location
+}) {
   try {
-    const result = await client.query(`
-      INSERT INTO users(username, password)
-      VALUES ($1, $2);
-    `, [username, password]);
+    const { rows } = await client.query(`
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password, name, location]);
 
-    return result;
+    return rows;
   } catch (error) {
     throw error;
   }
 }
 
+async function getAllUsers() {
+  const { rows } = await client.query(`SELECT id, username FROM users;`);
 
+  return rows;
+}
 
-
-
-module.exports = {
+module.exports = {  
   client,
-  getAllUsers,
   createUser,
- 
+  getAllUsers,
 }
--- db/seed.js
+++ db/seed.js
@@ -1,24 +1,4 @@
-
-const {
-  client,
-  getAllUsers,
-  createUser,
-} = require('./index');
-
-async function createInitialUsers() {
-  try {
-    console.log("Starting to create users...");
-    
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
-    console.log(albert);
-
-    console.log("Finished creating users!");
-  } catch(error) {
-    console.error("Error creating users!");
-    throw error;
-  }
-}
+const { client, getAllUsers, createUser } = require("./index");
 
 async function dropTables() {
   try {
@@ -43,13 +23,32 @@ async function createTables() {
       CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
-      );
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+location VARCHAR(255) NOT NULL,
+active BOOLEAN DEFAULT true
+      
+      )
     `);
 
     console.log("Finished building tables!");
   } catch (error) {
     console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+
+    await createUser({ username: "albert", password: "bertie99", name:'', location:'' });
+    await createUser({ username: "sandra", password: "2sandy4me", name:'', location:''  });
+    await createUser({ username: "glamgal", password: "soglam", name:'', location:''  });
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
     throw error;
   }
 }
@@ -79,7 +78,6 @@ async function testDB() {
     throw error;
   }
 }
-
 
 rebuildDB()
   .then(testDB)

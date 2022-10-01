1e0344127bec4ce066955db88ec9d7384cecfc2a
Ross <rosskeene196@gmail.com>
2022-10-01 07:51:29 -0500

    start of part 3 post

--- api/posts.js
+++ api/posts.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const postsRouter = express.Router();
 const { getAllPosts } = require('../db');
+const { requireUser } = require('./utils');
+
 
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
   
     next()
   });
-
+  postsRouter.post('/', requireUser, async (req, res, next) => {
+    res.send({ message: 'under construction' });
+  });
   postsRouter.get('/', async (reg, res) => {
     const posts = await getAllPosts();
 
